5e6c4390bc841f8eb726ca0cb0744ae85bdc4276
luckysf <1170847775@qq.com>
2022-04-06 20:19:51 +0800

    更新结构

--- main.go
+++ main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+
+	mongo "gopkg.in/mgo.v2"
 )
 
 // 日志参数
-const (
-	code    = "code"
-	message = "message"
-)
+// const (
+// 	code    = "code"
+// 	message = "message"
+// )
 
 func main() {
-	fmt.Println()
-	// 初始化日志
+
+	session, err := mongo.Dial("114.117.198.50")
+	if err != nil {
+		return
+	}
+
+	fmt.Println(session)
+
+	// // 初始化日志
+	// router := gin.New()
+	// err := router.Run("127.0.0.1:80")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
 	// log, err := logs.NewLog()
 	// if err != nil {
